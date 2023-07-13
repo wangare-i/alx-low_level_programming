8ab5ab0449070ace10da440d4e6f8fb1bdafa402
wanagre-i <wangareirungu3@gmail.com>
2023-07-13 07:04:36 -0700

    heyyy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  *main - Entry point
  *Return: always 0 (success)
  */
-int main(void)
+int main(void);
 { 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
