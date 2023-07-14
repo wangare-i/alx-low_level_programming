885b33626208409be3d7f336b81231214812a03f
wanagre-i <wangareirungu3@gmail.com>
2023-07-14 00:02:45 -0700

    @hello@

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,13 @@
  */
 int main(void)
 {
+     char:a;
+     int:b;
+     long:c;
+     long long int:d;
+     float:f;
+
+
 	print("Size of a char: %d byte9s)", sizeof(char));
 	printf("Size of an int: %d byte(s)", sizeof(int));
 	printf("Size of a long int: %d byte(s)", sizeof(long int));
